068f45a6b4f1e94b9d3d33ab95ec6db9b65d2df2
Elliot Wright <wright.elliot@gmail.com>
2014-03-11 22:33:01 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 
-// This is still horrible! Get variables are naughty without validations and sanitisation
 $module     = $_GET['module'];
 $controller = ucfirst(strtolower($_GET['controller'])) . "Controller";
 $action     = ucfirst(strtolower($_GET['action'] . "Action"));
+
+// Let's say for arguments sake then that you've gone to:
+//
+// index.php?module=admin&controller=admin&action=home
+//
+// Ignore the fact that this is horrificially insecure 
+// (running code on demand from the URL without validation!)
+// you would be accessing:
+//
+// modules/admin/Controller/AdminController.php
+//
+// You'd then be making a new object (new $controller()) which
+// would be a new AdminController(). and then calling the method
+// homeAction(). 
+//
+// So, if you want to know what stuff is going on, check that file
+// out too, go have a look at it!
 
 $file = realpath(dirname(__FILE__)) . "/modules/" . $module . "/Controller/" . $controller . ".php";
 
